dc73850b36e8c7554912715b74e1e62b06aae4cc
beanjs <445212619@qq.com>
2024-10-23 23:30:37 +0800

    delete dist dir

--- dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { NrfMeshPlugin } from './definitions';
-declare const NrfMesh: NrfMeshPlugin;
-export * from './definitions';
-export { NrfMesh };
--- dist/esm/index.js
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const NrfMesh = registerPlugin('NrfMesh', {
-    web: () => import('./web').then(m => new m.NrfMeshWeb()),
-});
-export * from './definitions';
-export { NrfMesh };
-//# sourceMappingURL=index.js.map
